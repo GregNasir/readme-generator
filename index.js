d5254bb332e8771a5732709c9775ad6a2f27fd1c
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-06-05 12:43:12 -0400

    added comments to index.js codes

--- index.js
+++ index.js
@@ -1,75 +1,12 @@
-// TODO: Include packages needed for this application
+// Required packages for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// const  = (data) => {
-//     return `<!DOCTYPE html>
-//     <html lang="en">
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//         <title>Document</title>
-//     </head>
-//     <body>
-//         <header style='color:white; background-color:${data.color}'>
-//             <h1># ${data.title}</h1>
-//             <p>by ${data.name}</p>
-//         </header>
-        
-//         <section>
-//             <h3># Table of Contents<h3>
-//             <p>* description<p>
-//             <p>* installation<p>
-//             <p>* Usage<p>
-//             <p>* Contributing<p>
-//             <p>* Tests<p>
-//             <p>* Questions<p>
-//         </section>
-
-//         <section>
-//             <h2># Description<h2>
-//             <p>${data.description}<p>
-//         </section>
-
-//         <section>
-//             <h2># Installation<h2>
-//             <p>${data.Installation}<p>
-//         </section>
-
-//         <section>
-//             <h2># Usage<h2>
-//             <p>${data.Usage}<p>
-//         </section>
-
-//         <section>
-//             <h2># Contributing<h2>
-//             <p>${data.Contributing}<p>
-//         </section>
-
-//         <section>
-//             <h2># Tests<h2>
-//             <p>${data.Tests}<p>
-//         </section>
-
-//         <section>
-//             <h2># License<h2>
-//             <p>${data.license}<p>
-//         </section>
-
-//         <section>
-//             <h2># Questions<h2>
-//             <p>${data.description}<p>
-//         </section>
-        
-//     </body>
-//     </html>`
-// }
 
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 
 const questions = [
     {
@@ -82,11 +19,7 @@ const questions = [
         name: "description",
         message: "Please describe your project and how its functionality.",
     },
-    // {
-    //     type: "input",
-    //     name: "Table of Contents",
-    //     message: "This is the table of contents.",
-    // },
+    
     {
         type: "input",
         name: "installation",
@@ -130,28 +63,13 @@ const questions = [
         message: "Provide your email.",
     },
 ];
-// .then((answers) => {
-//     const document = generateMarkdown({ ...answers});
-//     fs.writeToFile('README.md', document, (err) =>
-//         err ? console.log(err) : console.log('Success!'))
-//     });
 
-
-
-
-
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     return fs.writeFileSync(path.join(process.cwd().fileName).data);
-
-// }
 function writeToFile(fileName, data) {
     const filePath = path.join(process.cwd(), fileName);
     fs.writeFileSync(filePath, data);
   }
 
-// TODO: Create a function to initialize app
+// function to initialize app
 function init() {
     inquirer.prompt(questions).then((answers) => {
         console.log('Professional README');
@@ -159,5 +77,5 @@ function init() {
     });
 }
 
-// Function call to initialize app
+// initializing app
 init();
